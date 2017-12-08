022ca74bfdaa8243cc827a084beb243985b448ca
Jeff Morgan <Jeff.Morgan@Cerner.com>
2017-12-08 17:51:08 -0600

    Final Project. Completed.

--- bin/project_class/core.clj
+++ bin/project_class/core.clj
@@ -4,11 +4,15 @@
   (use ['clojure.string :only '(split)])
 (require '[com.climate.claypoole :as cp])
 (require ['clojure.string :as 'str])
+(import java.util.concurrent.Executors)
+(import '[java.util.concurrent Executors ExecutorService Callable])
 
 (defn str-2-int
+  ;;parse read-string to List;;
   [string]
   (map #(Integer/parseInt %)
        (split string #" ")))
+
 (defn read-lines []
   "Reads by line from File"
   (with-open [rdr (reader (str "resources/" (read-line)))]
@@ -31,53 +35,52 @@
 (defn split-4 [x]
   (apply concat (pmap split-2(split-2 x))))
 (defn split-8 [x]
-  (apply concat (pmap split-2(split-4 x))))
+  (apply concat (pmap split-4(split-2 x))))
 (defn split-16 [x]
-  (apply concat (pmap split-2(split-8 x))))
+  (apply concat (pmap split-8(split-2 x))))
 (defn split-32 [x]
-  (apply concat (pmap split-2(split-16 x))))
+  (apply concat (pmap split-16(split-2 x))))
 
 (defn choice [x]
   (cond
     (= (count x) 2) (max (first x), (last x))
-    :else (max (+ (first x) (min (choice (rest (butlast x))),  (choice(rest(rest x))))),
-               (+ (last x) (min (choice (butlast( butlast x))), (choice(rest (butlast x))))))))
+    :else (max (+ (first x) (min ( choice (rest (butlast x))), ( choice(rest(rest x))))),
+               (+ (last x) (min ( choice (butlast( butlast x))), ( choice(rest (butlast x))))))))
 
 (println "Welcome, To the Crazy Game: ")
-
+(println "Enter file size you wish to play. (add .txt on the end)")
 (defn scoreSingle [x]
   (println "Player 1 will receive" (time(choice x))
            "Player 2 gets" (- (reduce + x) (choice x) )))
 (defn scoreDouble [x]
-  (println "Player 1 will receive" (reduce +(pmap choice(split-2 x))) 
+  (println "Player 1 will receive" (time (reduce +(pmap choice(split-2 x))))
            "Player 2 gets" (- (reduce + x) (reduce +(pmap choice(split-2 x))))))
 (defn score4 [x]
-  (println "Player 1 will receive" (reduce +(pmap choice(split-4 x))) 
+  (println "Player 1 will receive" (time (reduce +(pmap choice(split-4 x))) )
            "Player 2 gets" (- (reduce + x) (reduce +(pmap choice(split-4 x))))))
 (defn score8 [x]
-  (println "Player 1 will receive" (reduce +(pmap choice(split-8 x))) 
+  (println "Player 1 will receive" (time (reduce +(pmap choice(split-8 x))) )
            "Player 2 gets" (- (reduce + x) (reduce +(pmap choice(split-8 x))))))
 (defn score16 [x]
-  (println "Player 1 will receive" (reduce +(pmap choice(split-16 x))) 
+  (println "Player 1 will receive" (time (reduce +(pmap choice(split-16 x))) )
            "Player 2 gets" (- (reduce + x) (reduce +(pmap choice(split-16 x))))))
 (defn score32 [x]
-  (println "Player 1 will receive" (reduce +(pmap choice(split-32 x))) 
+  (println "Player 1 will receive" (time (reduce +(pmap choice(split-32 x))) )
            "Player 2 gets" (- (reduce + x) (reduce +(pmap choice(split-32 x))))))
 
-;(println "Enter file for Single-Threaded Task")
-;(scoreSingle (workingList))
-;
-;(println "Enter file for 2-Threaded Task")
-;(scoreDouble (workingList))
-;
-;(println "Enter File for 4-Threaded Task")
-;(score4 (workingList))
-;
-;(println "Enter File for 8-Threaded Task")
-;(score8 (workingList))
-;
-;(println "Enter File for 16-Threaded Task")
-;(score16 (workingList))
-;
-;(println "Enter File for 32-Threaded Task")
-;(score32 (workingList))
+(scoreSingle (workingList))
+(println "Enter file size you wish to play. (add .txt on the end)")
+(scoreDouble (workingList))
+(println "Enter file size you wish to play. (add .txt on the end)")
+(score4 (workingList))
+(println "Enter file size you wish to play. (add .txt on the end)")
+(score16 (workingList))
+(println "Enter file size you wish to play. (add .txt on the end)")
+(score32 (workingList))
+
+;(defn test-prog [nthreads]
+;  (let [pool (Executors/newFixedThreadPool nthreads)
+;        tasks (choice(workingList))]
+;      (let [ret (.invokeAll pool tasks)]
+;        (.shutdown pool)
+;        (map #(.get %) ret))))
--- project.clj
+++ project.clj
@@ -4,4 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [com.climate/claypoole "1.1.4"]])
+                 [com.climate/claypoole "1.1.4"]
+                 [cc.qbits/knit "0.3.1"]])
